ca09b5c38f6cbe056dffae4c335953333ff02b19
HartnettMatt <matthew.e.hartnett@gmail.com>
2019-10-31 18:48:09 -0600

    Begin Work on assigment 8

---
+++ assignment-8/Graph.cpp
@@ -0,0 +1,33 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include "Graph.hpp"
+#include<vector>
+#include<iostream>
+using namespace std;
+
+void Graph::addEdge(string v1, string v2){
+
+}
+
+void Graph::addVertex(string name){
+
+}
+
+void Graph::displayEdges(){
+
+}
+
+void Graph::breadthFirstTraverse(string sourceVertex){
+
+}
+
+int Graph::getConnectedComponents(){
+
+}
+
+bool Graph::checkBipartite(){
+
+}
+
+
+#endif // GRAPH_H
